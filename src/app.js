34f1327182f9c557049481e04699c93e8a7416ac
ijai <chindaijai49@gmail.com>
2026-09-21 09:51:24 +0000

    feat: add ratelimiter and logger

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ app.use(express.json());
 const userRoute = require("./routes/user.route");
 const role = require("./routes/role.route");
 const productRoute = require("./routes/product.route");
+const logger = require("./middleware/logger");
+const limiter = require("./middlewares/rateLimiter");
+
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -15,5 +18,8 @@ app.get("/", (req, res) => {
 app.use("/user", userRoute);
 app.use("/role", role);
 app.use("/product", productRoute);
+
+app.use(logger);
+app.use(limiter);
 
 module.exports = app;
